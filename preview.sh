87c54fd1c2627d4382edba3193c808205d88cd44
cizordj <32869222+cizordj@users.noreply.github.com>
2022-03-06 15:17:28 -0300

    Update the preview script

--- preview.sh
+++ preview.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-
-DIR="$(dirname "$0")"
+cd "$(dirname "$(realpath "$0")")" || exit 1
 polybar-msg cmd quit
 
 # Launch the preview bar
-polybar -q top -c "$DIR"/preview.ini &
-polybar -q mid -c "$DIR"/preview.ini &
-polybar -q bottom -c "$DIR"/preview.ini &
+polybar -q top -c preview.ini &
+polybar -q mid -c preview.ini &
+polybar -q bottom -c preview.ini &
