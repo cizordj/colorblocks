f7672436849c42cd66c25af9de7ccd064a5f55db
cizordj <32869222+cizordj@users.noreply.github.com>
2022-03-29 22:36:44 -0300

    Fix a little bug with the kitty terminal
    When the launcher opens the kitty terminal it actually starts from
    /usr/local/share/colorblocks instead of the home directory. This little
    patch changes the directory to the user's home so it will fix this
    problem

--- scripts/launcher.sh
+++ scripts/launcher.sh
@@ -2,5 +2,6 @@
 cd "$(dirname "$0")" || exit
 SDIR="$(./sdir.sh)"
 THEME_FILE="$SDIR/rofi/launcher.rasi"
+cd "$HOME"
 rofi -no-config -no-lazy-grab -show drun \
     -modi drun -theme "$THEME_FILE"
--- scripts/run.sh
+++ scripts/run.sh
@@ -2,4 +2,5 @@
 cd "$(dirname "$0")" || exit
 SDIR="$(./sdir.sh)"
 THEME_FILE="$SDIR/rofi/launcher.rasi"
+cd "$HOME"
 rofi -no-config -no-lazy-grab -show run -modi run -theme "$THEME_FILE"
